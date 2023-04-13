7c8170f5f03d672fd02c7b5e7ae73918db764736
Sleepy Cat <gilb2966@gmail.com>
2023-04-13 16:28:28 -0500

    added addtl bootstrap styling

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 var apiKey = "faf3069d4928546472d6c4fe7cde6b6d"
+
+
+
+function searchcity () {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("#searchbtn").on("click", function (e) {
+  e.preventDefault()
+  searchcity()
+})
