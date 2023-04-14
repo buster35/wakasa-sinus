96db3834f63e97e81e2065029e74a21a422495f5
buster35 <gilb2966@gmail.com>
2023-04-14 18:31:45 -0500

    function for five day forecast

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ let citySearch = $(".form-control");
 
 function searchcity () {
   let requestCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
-
-  let showForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey; //geocoding api in order to convert city or zip code to lat/long to fetch information in the forecast api;https://openweathermap.org/api/geocoding-api//
-
   fetch(requestCurrent)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    console.log(data)
+  });
+}
+
+function fiveDay () {
+  let showForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey; //geocoding api in order to convert city or zip code to lat/long to fetch information in the forecast api;https://openweathermap.org/api/geocoding-api//
+  fetch(showForecast)
   .then(function (response) {
     return response.json()
   })
