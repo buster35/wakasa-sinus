50406161b7565216cc442f30a60f5b8949a8b231
Sleepy Cat <gilb2966@gmail.com>
2023-04-15 08:17:04 -0500

    added to first fetch fn

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let apiKey = "6f86c92e5bf1e1464a6de7b897f7f7dd";
 let searchBtn = document.getElementById("searchbtn");
-let citySearch = document.getElementById("#form").formcontrol.text; //
+
+ //
 
 function searchcity () {
+  let citySearch = document.getElementById("input").value;
   let requestCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
   fetch(requestCurrent)
   .then(function (response) {
     return response.json()
   })
   .then(function (data) {
-    console.log(data)
+    console.log(data) //returns an object; now that we have it, if we know how to access the object we can display that data//
+    populateCurrent() //we will grab info and set it on page using this fn
   });
 }
 
@@ -24,26 +27,20 @@ function fiveDay () {
   });
 }
 
+function populateCurrent () {
+  
+}
 
-//first, lets get the first fetch function to work
+
+
+
+
+
+
 //i want to be able to create variables for each of the divs i want to add text to
 
 // const forecastArray = sample.list.filter( (_dayObj, idx) => idx % 8 === 0) //this awesome line of code provided by our instructor to help 
 // console.log(forecastArray)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
